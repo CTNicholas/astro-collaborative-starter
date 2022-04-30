409986755772390abb7646d1cb3af8eae299487b
CTNicholas <33033422+CTNicholas@users.noreply.github.com>
2022-04-30 19:54:59 +0100

    feat: Each property now allows for multiple input elements

--- src/lib/live-user-form.ts
+++ src/lib/live-user-form.ts
@@ -13,7 +13,7 @@ export class LiveUserForm extends SelfAndOthersClass {
   visible: boolean = false
 
   get userInputs () {
-    const find = x => this.querySelector(`input[name="user-${x}"]`) as HTMLInputElement
+    const find = x => this.querySelectorAll(`input[name="user-${x}"]`) as NodeList
     return {
       name: find('name'),
       color: find('color'),
@@ -29,23 +29,27 @@ export class LiveUserForm extends SelfAndOthersClass {
   }
 
   attachListeners () {
-    Object.entries(this.userInputs).forEach(([key, inputElement]) => {
-      if (!inputElement) {
+    Object.entries(this.userInputs).forEach(([key, inputElements]) => {
+      if (!inputElements) {
         return
       }
 
-      inputElement.addEventListener('input', () => {
-        const room: LiveblocksRoom = document.querySelector('liveblocks-room')
-        room.setUser({ [key]: inputElement.value })
+      inputElements.forEach((inputElement: HTMLInputElement) => {
+        inputElement.addEventListener('input', () => {
+          const room: LiveblocksRoom = document.querySelector('liveblocks-room')
+          room.setUser({ [key]: inputElement.value })
+        })
       })
     })
   }
 
   updateInputValues () {
-    Object.entries(this.userInputs).forEach(([key, inputElement]) => {
-      if (this.self[key] && inputElement) {
-        inputElement.value = this.self[key]
-      }
+    Object.entries(this.userInputs).forEach(([key, inputElements]) => {
+      inputElements.forEach((inputElement: HTMLInputElement) => {
+        if (this.self[key] && inputElement) {
+          inputElement.value = this.self[key]
+        }
+      })
     })
   }
 
